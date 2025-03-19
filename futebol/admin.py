7e5ca237bba029b6c2f957ce3123af0a03584f82
diogorafaelbuarque <diogo.buarque@outlook.com>
2025-03-19 20:48:31 -0300

    segundo commit

--- futebol/admin.py
+++ futebol/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import Jogador
+from .models import Jogador, Time, MeuTime, Jogo
 
 # Register your models here.
 class JogadorAdmin(admin.ModelAdmin): #classe administrdora
-    list_display = ('nome','salario_inicial')
+    list_display = ('nome','salario_inicial','CPF',)
     search_fields = ('nome',)
     list_per_page = int = 2
     model = Jogador #modelo de referencia
 
+class JogoAdmin (admin.ModelAdmin):
+    list_display= ('timeA', 'timeA_gols', 'timeB_gols','timeB')
+    model = Jogo
+
 admin.site.register(Jogador, JogadorAdmin)
+admin.site.register(Time)
+admin.site.register(MeuTime)
+admin.site.register(Jogo, JogoAdmin)
--- futebol/models.py
+++ futebol/models.py
@@ -4,6 +4,28 @@ from django.db import models
 class Jogador(models.Model):
     nome=models.CharField(max_length=50)
     salario_inicial = models.FloatField()
+    CPF = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
+
+class Time(models.Model):
+    nome = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
+
+class MeuTime(models.Model):
+    players = models.ManyToManyField(Jogador)
+    def __str__(self):
+        return[jogador_atual.nome for jogador_atual in self.players.all()].__str__()
+
+class Jogo(models.Model):
+    data = models.DateTimeField()
+    timeA = models.ForeignKey(Time, on_delete=models.PROTECT, related_name="timeA")
+    timeA_gols = models.IntegerField(default=0)
+    timeB = models.ForeignKey(Time, on_delete=models.PROTECT, related_name="timeB")
+    timeB_gols  = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.timeA}x{self.timeB}'
